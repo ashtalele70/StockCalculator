b5a28648f3083b84194001845817d99a594352fd
Ashwini Talele <ashtalele70@gmail.com>
2020-04-12 17:14:40 -0700

    Corrected

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
-@app.route("ec2-52-53-165-168.us-west-1.compute.amazonaws.com:5000/home")
+@app.route("/home")
 def home():
     return render_template('LandingPage.html')
 
-@app.route("ec2-52-53-165-168.us-west-1.compute.amazonaws.com:5000/", methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
 def index():
     if(request.method == 'POST'):
         some_json = request.get_json()
@@ -13,10 +13,10 @@ def index():
     else:
         return jsonify({'about': 'Hello World'})
 
-@app.route('ec2-52-53-165-168.us-west-1.compute.amazonaws.com:5000/multi/<int:num>', methods=['GET'])
+@app.route('/multi/<int:num>', methods=['GET'])
 def get_multiply10(num):
     return jsonify({'result': num * 10})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="52.53.165.168", port=5000, debug=True)
